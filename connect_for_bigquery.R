fbc5533b32b4a24ee3b4c7490821cd956184ef65
tminoda <tminoda@gmail.com>
2014-09-28 15:59:57 +0900

    First Edition
    Initial Version

---
+++ connect_for_bigquery.R
@@ -0,0 +1,27 @@
+##########
+# Connection From R to BigQuery
+# Created:2014/09/28
+
+# Hadley's git hub page
+# https://github.com/hadley/bigrquery
+##########
+
+
+# install packages
+install.packages("devtools")
+install.packages("httpuv")
+
+library(devtools)
+library(httpuv)
+
+# devtools
+devtools::install_github("hadley/devtools")
+# bigquery
+devtools::install_github("hadley/bigrquery")
+library(bigrquery)
+
+# Run query
+prj <- "ace-scarab-713"
+sql <- "SELECT  * FROM [publicdata:samples.wikipedia] LIMIT 1000"
+results <- query_exec(sql , project = prj)
+
